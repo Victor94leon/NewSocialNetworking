938430db5a4db6940597592f152dc1ae255c4136
Víctor <victorlopez94leon@gmail.com>
2016-05-16 13:09:42 +0200

    Refactorización de la clase NewsFeed para evitar repetir código

--- NewsFeed.java
+++ NewsFeed.java
@@ -1,60 +1,30 @@
 import java.util.ArrayList;
 public class NewsFeed
-{
-    private ArrayList<MessagePost> messages;
-    private ArrayList<PhotoPost> photos;
-    private ArrayList<JoinGroupPost> groups;
+{   
+    private ArrayList<Post> posts;
 
     /**
      * Constructor for objects of class NewsFeed
      */
     public NewsFeed()
     {
-        messages = new ArrayList<>();
-        photos = new ArrayList<>();
-        groups = new ArrayList<>();
+        posts = new ArrayList<>();
     }
 
     /**
-     * Add a post with a message
-     * 
-     * @message the message to introduce
+     * Añade un Post
      */
-    public void addMessagePost(MessagePost message)
+    public void addPost(Post post)
     {
-        messages.add(message);
-    }
-
-    /**
-     * Add a post with a image
-     * 
-     * @message the image to introduce
-     */
-    public void addPhotoPost(PhotoPost photo)
-    {
-        photos.add(photo);
-    }
-
-    /**
-     * Añade un JoinGroupPost
-     */
-    public void addMessagePost(JoinGroupPost group)
-    {
-        groups.add(group);
+        posts.add(post);
     }
 
     /**
      * Show the post
      */
     public void show(){
-        for(MessagePost message : messages){
-            message.display();
-        }
-        for(PhotoPost photo : photos){
-            photo.display();
-        }
-        for (JoinGroupPost group : groups) {
-            group.display();
+        for(Post postEnLista : posts){
+            postEnLista.display();
         }
     }
 }
